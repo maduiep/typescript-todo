0ae2fd1f9b4f58fef52c43cff05b4cb261962570
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2022-03-11 16:35:44 +0100

    added todo function props

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './styles.css'
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({todo, setTodo}: Props) => {
   return (
    <form action="#" className="input">
        <input type="input" placeholder='Ener a task' className='input__box'/>
        <button className="input_submit" type='submit'>Go</button>
+
    </form>
   )
 }
