29611c302f553bcbd1b687a48ccd278ecead1ea9
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2022-03-11 16:19:49 +0100

    completed input field box

---
+++ src/components/InputField.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+const InputField = () => {
+  return (
+   <form action="#" className="input">
+       <input type="input" placeholder='Ener a task' className='input__box'/>
+       <button className="input_submit" type='submit'>Go</button>
+   </form>
+  )
+}
+
+export default InputField
