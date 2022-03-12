c2a0d87b20c9ef269f567d7c26fd631f1e648eba
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2022-03-12 04:24:13 +0100

    added single list item compponents

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
 import './styles.css'
 
 
@@ -12,7 +13,12 @@ const TodoList: React.FC<Props> = ({todos, setTodos}) => {
   return (
     <div className='todos'>
     {todos.map((todo) => (
-        <li>{todo.todo}</li>
+        <SingleTodo 
+        todo={todo} 
+        key={todo.id} 
+        todos={todos}
+        setTodos={setTodos}
+        />
     ))}
     </div>
   )
